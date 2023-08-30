a9c0b53dd31313144694dcff2f99a16ad039cc05
matyalatte <matyalatte@gmail.com>
2023-08-30 20:10:05 +0900

    add OnFilesDropped event for uiEntry

--- windows/uipriv_windows.hpp
+++ windows/uipriv_windows.hpp
@@ -28,6 +28,7 @@ extern void uninitAlloc(void);
 extern BOOL runWM_COMMAND(WPARAM wParam, LPARAM lParam, LRESULT *lResult);
 extern BOOL runWM_NOTIFY(WPARAM wParam, LPARAM lParam, LRESULT *lResult);
 extern BOOL runWM_HSCROLL(WPARAM wParam, LPARAM lParam, LRESULT *lResult);
+extern BOOL runWM_DROPFILES(WPARAM wParam, LPARAM lParam, LRESULT *lResult);
 extern void issueWM_WININICHANGE(WPARAM wParam, LPARAM lParam);
 
 // utf16.cpp
